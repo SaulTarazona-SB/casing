347b89d3102b740413d8d81b51f19a76a286f10f
SaulTarazona-SB <saul.tarazona@segurosbolivar.com>
2021-05-20 09:52:01 -0500

    Cambio de nombre de la funcionalidad por servicioPalabras

--- src/main/java/com/bolivar/comunes/casing/controller/PalabrasAjustadasController.java
+++ src/main/java/com/bolivar/comunes/casing/controller/PalabrasAjustadasController.java
@@ -14,42 +14,42 @@ public class PalabrasAjustadasController {
     @Autowired
     private PalabrasAjustadasService palabrasService;
 
-    @PostMapping("/api/v1/palabras/textoAjustado")
+    @PostMapping("/api/v1/servicioPalabras/textoAjustado")
     public ResponseEntity<ObtenerpalabraajustadaResponse> ajustarTexto1MayusculaDemasMinuscula(@RequestParam String palabra, @RequestParam String token) {
         return ResponseEntity.ok(palabrasService.ajustarTexto1MayusculaDemasMinuscula(palabra, token));
     }
 
-    @PostMapping("/api/v1/palabras/invertido")
+    @PostMapping("/api/v1/servicioPalabras/invertido")
     public ResponseEntity<ObtenerpalabraajustadaResponse> intercalarMayusculasMinusculas(@RequestParam String palabra) {
         return ResponseEntity.ok(palabrasService.intercalarMayusculasMinusculas(palabra));
     }
 
-    @PostMapping("/api/v1/palabras/primeraLetra")
+    @PostMapping("/api/v1/servicioPalabras/primeraLetra")
     public ResponseEntity<ObtenerpalabraajustadaResponse> cambiarLetra1DemasDiferenteALetra1(@RequestParam String palabra) {
         return ResponseEntity.ok(palabrasService.cambiarLetra1DemasDiferenteALetra1(palabra));
     }
 
-    @PostMapping("/api/v1/palabras/primeraLetraYTexto")
+    @PostMapping("/api/v1/servicioPalabras/primeraLetraYTexto")
     public ResponseEntity<ObtenerpalabraajustadaResponse> cambiarLetra1DemasIgualALetra1(@RequestParam String palabra) {
         return ResponseEntity.ok(palabrasService.cambiarLetra1DemasIgualALetra1(palabra));
     }
 
-    @PostMapping("/api/v1/palabras/mayusculasTokenLetras")
+    @PostMapping("/api/v1/servicioPalabras/mayusculasTokenLetras")
     public ResponseEntity<ObtenerpalabraajustadaResponse> cambiarTodoMayusculasTokenEntreLetras(@RequestParam String palabra, @RequestParam String token) {
         return ResponseEntity.ok(palabrasService.cambiarTodoMayusculasTokenEntreLetras(palabra, token));
     }
 
-    @PostMapping("/api/v1/palabras/minusculasTokenLetras")
+    @PostMapping("/api/v1/servicioPalabras/minusculasTokenLetras")
     public ResponseEntity<ObtenerpalabraajustadaResponse> cambiarTodoMinusculasTokenEntreLetras(@RequestParam String palabra, @RequestParam String token) {
         return ResponseEntity.ok(palabrasService.cambiarTodoMinusculasTokenEntreLetras(palabra, token));
     }
 
-    @PostMapping("/api/v1/palabras/mayusculasTokenPalabras")
+    @PostMapping("/api/v1/servicioPalabras/mayusculasTokenPalabras")
         public ResponseEntity<ObtenerpalabraajustadaResponse> cambiarTodoMayusculasTokenEntrePalabras(@RequestParam String palabra, @RequestParam String token) {
         return ResponseEntity.ok(palabrasService.cambiarTodoMayusculasTokenEntrePalabras(palabra, token));
     }
 
-    @PostMapping("/api/v1/palabras/minusculasTokenPalabras")
+    @PostMapping("/api/v1/servicioPalabras/minusculasTokenPalabras")
     public ResponseEntity<ObtenerpalabraajustadaResponse> cambiarTodoMinusculasTokenEntrePalabras(@RequestParam String palabra, @RequestParam String token) {
         return ResponseEntity.ok(palabrasService.cambiarTodoMinusculasTokenEntrePalabras(palabra, token));
     }
